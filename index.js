6c46dfd0666647796953cc19bf1fb25204291d32
chakri-m <56298153+chakri-m@users.noreply.github.com>
2020-12-11 14:04:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const port = 3100;
 
-app.get('/', (req, res) => res.send('Well done guys, your Nodejs App is up and running! \n With love, Your Coach '));
+app.get('/', (req, res) => res.send('Well done guys, your Nodejs App is up and running!'));
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
